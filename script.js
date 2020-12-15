5bed7df484c6b9aac394f6a59cdcb52465d33683
fionayuqizhang <yu.qi.zhang@ocsbstudent.ca>
2020-12-15 15:36:20 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let area=(6+4)/2*5;
+let area=(7+3)/2*4;
+let area=(5+9)/2*6;
+function myFunction() {
+  trapezoid=document.getElementById("myText").value;
+   trapezoid=document.getElementById("bText").value;
+    trapezoid=document.getElementById("hText").value;
+  trapezoid=(a+b)/2*h;
+  alert(trapezoid);
+}
+function iFunction() {
+  let trapezoid = document.getElementById("myText").value;
+  document.getElementById("demo").innerHTML = trapezoid;
+}
