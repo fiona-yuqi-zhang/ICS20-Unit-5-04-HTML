13f94a3fe6f555647c73d6975b98ade953105ea4
fionayuqizhang <yu.qi.zhang@ocsbstudent.ca>
2020-12-15 15:50:19 +0000

    1

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+let base1=(6+4)/2*5;
+let base2=(7+3)/2*4;
+let height=(5+9)/2*6;
 let area=(6+4)/2*5;
-let area=(7+3)/2*4;
-let area=(5+9)/2*6;
 function myFunction() {
-  trapezoid=document.getElementById("myText").value;
-   trapezoid=document.getElementById("bText").value;
-    trapezoid=document.getElementById("hText").value;
-  trapezoid=(a+b)/2*h;
+  base1=document.getElementById("myText").value;
+   base2=document.getElementById("bText").value;
+    height=document.getElementById("hText").value;
+    base1=parseInt(base1,10);
+     base2=parseInt(base2,10);
+      height=parseInt(height,10);
+  trapezoid=(base1+base2)/2*height;
   alert(trapezoid);
 }
 function iFunction() {
