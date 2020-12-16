9015c435c23a09a4a18d54f43d195cc2dff21292
fionayuqizhang <yu.qi.zhang@ocsbstudent.ca>
2020-12-16 00:15:39 +0000

    1

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let base2=(7+3)/2*4;
 let height=(5+9)/2*6;
 let area=(6+4)/2*5;
 function myFunction() {
-  base1=document.getElementById("myText").value;
-   base2=document.getElementById("bText").value;
-    height=document.getElementById("hText").value;
+  base1=document.getElementById("my-text").value;
+   base2=document.getElementById("b-text").value;
+    height=document.getElementById("h-text").value;
     base1=parseInt(base1,10);
      base2=parseInt(base2,10);
       height=parseInt(height,10);
@@ -13,6 +13,6 @@ function myFunction() {
   alert(trapezoid);
 }
 function iFunction() {
-  let trapezoid = document.getElementById("myText").value;
+  let trapezoid = document.getElementById("my-text").value;
   document.getElementById("demo").innerHTML = trapezoid;
 }
